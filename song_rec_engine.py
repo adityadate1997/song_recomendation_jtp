996c8d92bad20ec21769736a688c450dd06ac260
adityadate1997 <60092460+adityadate1997@users.noreply.github.com>
2020-06-10 20:36:13 +0530

    Engine with function

--- song_rec_engine.py
+++ song_rec_engine.py
@@ -1,29 +1,56 @@
-import pandas as pd 
-import numpy as np 
-from scipy import sparse 
+import pandas as pd
+import numpy as np
+from scipy import sparse
 from sklearn.neighbors import NearestNeighbors
 
-df = pd.read_csv('top10s.csv', encoding='latin1')
-#print(df.head())
-# print(df.isnull().values.any())
+df = pd.read_csv('top10s.csv',encoding='latin1')
 
-df_for_model = df[['bpm', 'nrgy', 'dnce', 'dB', 'live', 'val', 'acous', 'spch', 'pop']]
-# print(df_for_model.head())
+# print(df.head())
+df.shape
 
-df_array = df_for_model.to_numpy()
-# print(df_array)
+df_to_model = df[['bpm','nrgy','dnce','val','spch', 'acous','dB']]
 
-df_spm = sparse.csr_matrix(df_array)
-# print(df_spm)
+df_ar = df_to_model.to_numpy()
 
-model_nn = NearestNeighbors(metric='euclidean', algorithm='brute', n_neighbors=5)
+df_spm = sparse.csr_matrix(df_ar)
+
+model_nn = NearestNeighbors(metric='euclidean',algorithm='brute',n_neighbors=5)
 
 model_nn.fit(df_spm)
 
-id = df.loc[df['title']=='TiK ToK'].index
 
-distances, indices = model_nn.kneighbors(df_spm[id], n_neighbors=5)
+def songRecommender(song_name):
 
-# print(indices[0][1])
-for i in indices:
-    print(df['title'][i])
+    id = df.loc[df['title'] == song_name]
+
+    print("\n You selected : \n", id[['title','top genre', 'artist']])
+
+
+    # values = model_nn.kneighbors(df_spm[id], n_neighbors=10)
+    rec_genre = df.loc[df['top genre'] == id['top genre'].values[0]]
+    rec_genre = rec_genre[rec_genre['title'] != id['title'].values[0]].nlargest(5,['pop','year'])
+
+
+    df_artist = df.loc[df['artist'] == id['artist'].values[0]]
+    rec_artist = df_artist[df_artist.index.isin(np.setdiff1d(df_artist.index,rec_genre.index))]
+    rec_artist = rec_artist[rec_artist['title'] != id['title'].values[0]].nlargest(5,['pop','year'])
+    # print(rec_artist['title'].values.difference(rec_genre['title'].values))
+
+
+   
+    # rec_genre.sort(['pop'], ascending=[1, 0])
+    print("\nTop In Genre: \n ",rec_genre[['title','top genre','artist']])
+
+    print("\nTop In Artist: \n ",rec_artist[['title','top genre','artist']])
+
+    values = model_nn.kneighbors(df_spm[id.index], n_neighbors=15)
+
+
+    recomendations = df[df.index.isin(values[1][0])] 
+    recomendations = recomendations[recomendations['top genre'] != id['top genre'].values[0]] 
+    recomendations = recomendations[recomendations['artist'] != id['artist'].values[0]]
+    print(" \n Discover : \n ", recomendations[['title', 'top genre', 'artist']].head())
+
+
+songRecommender('Sucker')
+
