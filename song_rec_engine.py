ebf2fd0fcea29c24fcb51ef6be664d8ba140a1cb
adityadate1997 <60092460+adityadate1997@users.noreply.github.com>
2020-06-11 11:21:26 +0530

    Update song_rec_engine.py

--- song_rec_engine.py
+++ song_rec_engine.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 from scipy import sparse
 from sklearn.neighbors import NearestNeighbors
+from tqdm import tqdm
 
 df = pd.read_csv('top10s.csv',encoding='latin1')
 
@@ -10,11 +11,18 @@ df.shape
 
 df_to_model = df[['bpm','nrgy','dnce','val','spch', 'acous','dB']]
 
+for i in df_to_model.columns:
+    try:
+        for j in tqdm(range(0,df.shape[0])):
+            df_to_model.loc[j,i] = (df_to_model.loc[j,i] - df_to_model[i].min())/(df_to_model[i].max() - df_to_model[i].min())
+    except:
+        pass
+
 df_ar = df_to_model.to_numpy()
 
 df_spm = sparse.csr_matrix(df_ar)
 
-model_nn = NearestNeighbors(metric='euclidean',algorithm='brute',n_neighbors=5)
+model_nn = NearestNeighbors(metric='cosine',algorithm='brute',n_neighbors=20)
 
 model_nn.fit(df_spm)
 
@@ -52,5 +60,5 @@ def songRecommender(song_name):
     print(" \n Discover : \n ", recomendations[['title', 'top genre', 'artist']].head())
 
 
-songRecommender('Sucker')
+songRecommender("Let Me Be Your Lover")
 
